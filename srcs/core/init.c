044dc75e03c8c8187b81007995e4163dfd24f93c
Joakim Raivio <jraivio@student.hive.fi>
2023-01-24 18:40:05 +0200

    Refactored code to fix norm bug

--- srcs/core/init.c
+++ srcs/core/init.c
@@ -6,7 +6,7 @@
 /*   By: jraivio <jraivio@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 13:46:04 by jraivio           #+#    #+#             */
-/*   Updated: 2023/01/24 16:12:32 by jraivio          ###   ########.fr       */
+/*   Updated: 2023/01/24 18:38:07 by jraivio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,21 @@ void	init_getters(void)
 void	init_mlx(void)
 {
 	t_image	*image;
+	t_frame	*frame;
 
-	get_frame()->mlx = mlx_init();
-	if (!(get_frame()->mlx))
+	frame = get_frame();
+	frame = mlx_init();
+	if (!(frame->mlx))
 		ft_panic("MLX initilization failed.");
-	get_frame()->window = mlx_new_window(get_frame()->mlx,
-		SCREEN_W, SCREEN_H, WINDOW_NAME);
-	if (!(get_frame()->window))
+	frame->window = mlx_new_window(frame->mlx,
+			SCREEN_W, SCREEN_H, WINDOW_NAME);
+	if (!(frame->window))
 		ft_panic("MLX window initialization failed.");
 	image = get_image();
-	image->image = mlx_new_image(get_frame()->mlx, SCREEN_W, SCREEN_H);
+	image->image = mlx_new_image(frame->mlx, SCREEN_W, SCREEN_H);
 	image->address = mlx_get_data_addr(image->image, &(image->bits_per_pixel),
 			&(image->line_length), &(image->endian));
-	mlx_expose_hook(get_frame()->window, on_expose, NULL);
+	mlx_expose_hook(frame->window, on_expose, NULL);
 }
 
 void	init(void)
